e3d352e8be558fecd144bbdc047c32930832b6b6
Binita rijal <rijalbinita01@gmail.com>
2026-01-12 14:51:57 +0545

    different controller, routes formed for auth

---
+++ controller/auth/authController.js
@@ -0,0 +1,65 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../../model/userModel');
+
+
+
+exports.registerUser = async (req, res) => {
+    try{
+const { userEmail, userPhonenumber, userPassword, role } = req.body;
+if(!userEmail || !userPhonenumber || !userPassword){
+    return  res.status(400).json({ message: 'All fields are required' });
+}
+//check if user already exists
+const existingUser = await User.findOne({ userEmail: userEmail });
+if (existingUser) {
+    return res.status(400).json({ message: 'User already exists' });
+}
+
+const hashedPassword =  bcrypt.hashSync(userPassword, 10);
+const response = await User.create({ userEmail,
+     userPhonenumber,
+      userPassword: hashedPassword,
+       role });
+if (response) {
+    res.status(201).json({ 
+        message: 'User registered successfully', 
+        data: response 
+    });  
+} else {
+    res.status(400).json({ message: 'User registration failed' });
+}
+    }catch(error){
+        console.error('Error during user registration:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { userEmail, userPassword } = req.body;
+
+        const user = await User.findOne({ userEmail });
+        if (!user) {
+            return res.status(400).json({ message: 'email not found' });
+        }
+
+
+        const isPasswordValid = bcrypt.compareSync(userPassword, user.userPassword);
+        if(isPasswordValid){
+const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+res.cookie('token', token, { httpOnly: true });
+
+        }
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        res.status(200).json({ message: 'Login successful', data: user });
+
+    } catch (error) {
+        console.error('Error during user login:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
---
+++ routes/auth/authRoutes.js
@@ -0,0 +1,5 @@
+const router=require('express').Router();
+const {registerUser,loginUser}=require('../../controller/auth/authController');
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+module.exports=router;
